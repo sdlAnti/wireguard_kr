43e283bc058e88af5f910962d1f044172cffd65a
Roman Potupin <sd--anti@yandex.ru>
2022-05-25 12:26:36 +0300

    Add README.md, wg.conf generation vs keys and peers section

---
+++ README.md
@@ -0,0 +1,3 @@
+# Docker wireguard
+Wireguard in an Ubuntu-based container vs. automatic generation of server and peer configuration
+> in progress
--- run.sh
+++ run.sh
@@ -1,53 +1,70 @@
-#!/bin/bash 
+#!/bin/bash -x
+n=1
+peers=5
 server_keygen () {
-	if [ -e server_private_key ]
-		then
-			cat server_private_key
-		else
-			wg genkey | tee server_private_key | wg pubkey > server_public_key
-			cat server_private_key
-	fi
-}
-
-peer_keygen () {
-        if [ -e peer_private_key ]
+        if [ -e server_private_key ]
                 then
-                        cat peer_public_key
+                        cat server_private_key
                 else
-                        wg genkey | tee peer_private_key | wg pubkey > peer_public_key
-                        cat peer_public_key
+                        wg genkey | tee server_private_key | wg pubkey > server_public_key
+                        chmod 600 server_private_key
+                        cat server_private_key
         fi
 }
 
+peer_keygen () {
+if [ -e peer"$n"_private_key ]
+    then
+        if [ -e "peer"$n"_public_key" ]
+            then
+                cat "peer"$n"_public_key"
+            else
+                cat peer"$n"_private_key | wg pubkey > "peer"$n"_public_key"
+                cat "peer"$n"_public_key"
+        fi
+    else
+        wg genkey | tee "peer"$n"_private_key" | wg pubkey > "peer"$n"_public_key"
+        chmod 600 "peer"$n"_private_key"
+        cat "peer"$n"_public_key"
+    fi
+}
+
 peer_generation () {
+while [ $n -le $peers ]
+do
 cat << EOF
 [Peer]
+#Peer$n
 PublicKey = $(peer_keygen)
-PresharedKey = 
+PresharedKey =
 AllowedIPs = 0.0.0.0/0
-Endpoint = demo.wireguard.com:51820
+Endpoint = test.tld:51820
 
 
 EOF
+(( n++ ))
+done
 }
 
 confgen () {
 cat << EOF > wg0.conf
 [Interface]
 Address = 10.10.10.1/24
+ListenPort = 51820
 PrivateKey = $(server_keygen)
+
+`echo 'PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE'`
+`echo 'PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE'`
 
 $(peer_generation)
 
-`echo 'PostUp = iptables -I OUTPUT ! -o %i -m mark ! --mark $(wg show %i fwmark) -m addrtype ! --dst-type LOCAL -j REJECT'`
-`echo 'PreDown = iptables -D OUTPUT ! -o %i -m mark ! --mark $(wg show %i fwmark) -m addrtype ! --dst-type LOCAL -j REJECT'`
 EOF
 }
 
-if [ -e 2.conf ]
-	then
-		echo conf file is exist
-	else
-		echo generating wireguard server and client conf
-		confgen
+if [ -e wg0.conf ]
+        then
+                echo conf file is exist
+        else
+                echo generating wireguard server and client conf
+                confgen
 fi
