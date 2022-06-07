370c11339ef12e699824d3747195284615a74c92
sd--anti <sd.anti.4835#gmail.com>
2022-06-07 17:29:04 +0300

    Pre-completed the client generation script, add simple and multiple client generation, added a check for the existence of a private and public key. The generation of the ip address "peer_ip" needs to be improved.

--- addpeer.sh
+++ addpeer.sh
@@ -1,29 +1,20 @@
 #!/bin/bash -x
-#PEERS=4
-PEERMANE=$1
-n=1
+PEERS=20
 qr_enable=0
-    if [ ! $PEER ]
-    then
-        echo "Peer name is empty"
-        echo "For generate PEER: addpeer.sh <peername>"
-        exit 0
-    fi 
-peer_generation () {
-    while [ $n -le $PEER ]
-    do
-        cat << EOF
-[Peer]
-#Peer$n
-PublicKey = $(peer_keygen)
-AllowedIPs = 10.10.10.$((1+$n))/32
+server_ip=`curl ifconfig.me`
+peer_dns=77.88.8.8
 
-EOF
-        peer_path=peers/peer"$n"
-        cat << EOF > $peer_path/peer"$n"_wg.conf
+peer_generation () {
+    peer_keygen
+    peer_path=peers/peer_"$n"
+    peer_ip=10.10.10.$(( `cat peer_ip.list | cut -f 4 -d '.' | tail -1` + 1 ))
+    echo "$peer_path"/peer_"$n"_wg.conf
+    echo pwd = $PWD
+
+    cat << EOF > $peer_path/peer_"$n"_wg.conf
 [Interface]
-PrivateKey = $(cat "$peer_path"/peer"$n"_private_key)
-Address = 10.10.10.$((1+$n))/32
+PrivateKey = $(cat "$peer_path"/peer_"$n"_private_key)
+Address = $peer_ip/32
 DNS = $peer_dns
 
 [Peer]
@@ -33,37 +24,61 @@ AllowedIPs = 0.0.0.0/0
 PersistentKeepalive = 20
 EOF
 
-        if [ $qr_enable -eq 1 ] && qrencode --version &> /dev/null
-            then
-                qrencode -t png -o "$peer_path"/peer"$n"_qr.png -r "$peer_path"/peer"$n"_wg.conf
-            else
-                echo "qrencode not installed" 
-                exit 1
-        fi
-
+        echo $peer_ip >> peer_ip.list
+        qr_generation
         (( n++ ))
-    done
 }
 peer_keygen () {
-peer_path=peers/peer"$n"
+peer_path=peers/peer_"$n"
 if [ -d $peer_path ]
     then
-        if [ -e peer"$n"_private_key ]
+        if [ -e "$peer_path"/peer_"$n"_private_key ]
             then
-                cat peer"$n"_private_key | wg pubkey > peer"$n"_public_key
-                cat peer"$n"_public_key
+                cat "$peer_path"/peer_"$n"_private_key | wg pubkey > "$peer_path"/peer_"$n"_public_key
+                cat "$peer_path"/peer_"$n"_public_key
 	    else
-            cd $peer_path
-	        wg genkey | tee peer"$n"_private_key | wg pubkey > peer"$n"_public_key
-        	chmod 600 peer"$n"_private_key
-	        cat peer"$n"_public_key
+	        wg genkey | tee "$peer_path"/peer_"$n"_private_key | wg pubkey > "$peer_path"/peer_"$n"_public_key
+        	chmod 600 "$peer_path"/peer_"$n"_private_key
+	        cat "$peer_path"/peer_"$n"_public_key
         fi
     else
         mkdir -p $peer_path
-        cd $peer_path
-        wg genkey | tee peer"$n"_private_key | wg pubkey > peer"$n"_public_key
-        chmod 600 peer"$n"_private_key
-        cat peer"$n"_public_key
+        wg genkey | tee "$peer_path"/peer_"$n"_private_key | wg pubkey > "$peer_path"/peer_"$n"_public_key
+        chmod 600 "$peer_path"/peer_"$n"_private_key
+        cat "$peer_path"/peer_"$n"_public_key
 fi
 }
-peer_generation
+qr_generation () {    
+    if [ $qr_enable -eq 0 ]
+        then
+            return 
+    elif [ $qr_enable -eq 1 ] && qrencode --version &> /dev/null
+        then
+            qrencode -t png -o "$peer_path"/peer_"$n"_qr.png -r "$peer_path"/peer_"$n"_wg.conf
+            qrencode -t ansiutf8 < "$peer_path"/peer_"$n"_wg.conf
+        else
+            echo "qrencode not installed" 
+            exit 1
+    fi
+}
+
+if [ $PEERS ]
+    then 
+        PEERNAME=${PEERS}
+        n=1
+        while [ $n -le $PEERNAME ]
+        do
+        peer_generation
+        done
+    else
+        PEERNAME=$1
+        n=$1
+        peer_generation
+        echo $PEERNAME
+        if [ ! $PEERNAME  ]
+            then
+                echo "Peer name is empty"
+                echo "For generate PEER: addpeer.sh <peername>"
+                exit 0
+        fi 
+fi
